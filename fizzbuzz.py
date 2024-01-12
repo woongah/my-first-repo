11e369149b8ae8d1f6b3290777570df4584f76de
woongah <w00n9ah@gmail.com>
2024-01-12 16:41:49 +0900

    fix: Adjust Priorty

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 def do_fizzbuzz():
     for i in range(1, 15+1):
-        if i%3==0:
+        if i%15==0:
+            print('fizzbuzz')
+        elif i%3==0:
             print('fizz')
         elif i%5==0:
             print('buzz')
-        elif i%15==0:
-            print('fizzbuzz')
         else:
             print(f'{i}')
 
